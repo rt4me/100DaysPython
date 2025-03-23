7f34f80b765b0a63796bc9f2851062df26e7ff65
rt4me <tabergin@gmail.com>
2025-03-23 13:26:27 -0500

    Add settings.json for linting customization; added sample Day27 file

--- Day27_tkinter/main.py
+++ Day27_tkinter/main.py
@@ -1,15 +1,17 @@
-from tkinter import * 
+# Using the below method of importing, you don't have to user tkintet.<method name>
+# and instead just use the method name (like Label or Button)
+from tkinter import *
 
 window = Tk()
 window.title("First GUI")
 window.minsize(width=500, height=300)
 
-#Label
+# Label
 
 my_label = Label(text="I Am a Label", font=("Arial", 24, "bold"))
-#Insert label and place at center bottom of window
+# Insert label and place at center bottom of window
 # https://docs.python.org/3/library/tkinter.html#the-packer
-my_label.pack() #pack is needed to place element on screen
+my_label.pack()  # pack is needed to place element on screen
 
 # Change the existing label text
 my_label["text"] = "New Text"
@@ -17,29 +19,23 @@ my_label["text"] = "New Text"
 my_label.config(text="New Text 2")
 
 
+# Button
 
-#Button
 
 def button_clicked():
     print("I got clicked")
+    # get() gets the value that was placed in the Entry field.
     my_label.config(text=input.get())
 
 
-button = Button(text="Click Me", command=button_clicked) #Note that this is the name of the fuction, so no parenthisis needed.
+button = Button(
+    text="Click Me", command=button_clicked)  # Note that this is the name of the function, so no parenthisis needed.
 button.pack()
 
 
-#Entry field
+# Entry field
 input = Entry(width=10)
 input.pack()
-
-
-
-
-
-
-
-
 
 
 window.mainloop()
