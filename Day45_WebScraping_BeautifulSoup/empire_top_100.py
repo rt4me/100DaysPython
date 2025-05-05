28b49da79b527fd2c085c721137c1a34887e42a0
Tim Bergin <tim.bergin@choreograph.com>
2025-05-05 12:00:10 -0500

    Complete Day 45; create Day30 directory

--- Day45_WebScraping_BeautifulSoup/empire_top_100.py
+++ Day45_WebScraping_BeautifulSoup/empire_top_100.py
@@ -4,21 +4,23 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
 import logging
+import re
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(filename)s:%(lineno)s - %(funcName)s() ] %(levelname)s %(message)s')
 
-base_url = 'https://www.empireonline.com/movies/features/best-movies-2/'
-filename = '100_top_movies.txt'
+URL = 'https://www.empireonline.com/movies/features/best-movies-2/'
+FILENAME = '100_top_movies.txt'
 
 def get_list_of_movies(url):
     
     logging.info(f"Fetch text from url: {url}.")
 
     try:
+        logging.info("Entering try block.")
         response = requests.get(f"{url}")
         page = response.text
-        soup_main = BeautifulSoup(page, "html.parser")
+        soup = BeautifulSoup(page, "html.parser")
         
     
     except requests.exceptions.RequestException as e:
@@ -26,14 +28,24 @@ def get_list_of_movies(url):
     except Exception as e:
         logging.error(f"Error processing URL {url}: {e}")
     
-    movie_list = soup_main.find_all('strong')
+    # Find all <strong> tags that start with a number
+    pattern = re.compile(r'^\d') # Matches strings starting with digit
+    elements_with_numbers = [tag.text for tag in soup.find_all('strong') if tag.text and pattern.match(tag.text)]
     
-    print(movie_list)
+    logging.info(f"Elements with numbers list: {elements_with_numbers}")
 
-    # with open(f"Day45_WebScraping_BeautifulSoup\\{filename}", "w", encoding="utf-8") as f:
-    #             f.write(text_content)
+    text_content = ""
+
+    # build String in reverse order
+    for movie in elements_with_numbers:
+          text_content = f"{movie}\n{text_content}"
+          
+
+    with open(f"Day45_WebScraping_BeautifulSoup\\{FILENAME}", "w", encoding="utf-8") as f:
+                 f.write(text_content)
     
     
     
-    if __name__ == "__main__":
-        get_list_of_movies(base_url)
+if __name__ == "__main__":
+    logging.info("Starting __main__ run.")
+    get_list_of_movies(URL)
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # 100DaysPython
 Projects associated with 100 Days of Python course on Udemy
+
+Run the following command to start the container on Work Laptop:
+docker run -it --rm -v "${PWD}:/app" 100dayspython_image bash
+
