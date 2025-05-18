c47dd7af5762de8faf420c259e4a48d0e9cf21c5
rt4me <tabergin@gmail.com>
2025-05-18 12:36:05 -0500

    Finish Day30 by updating Day29 code.

--- Day29_Password_Manager/main.py
+++ Day29_Password_Manager/main.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from tkinter import messagebox
 from password_gen import PassGen as pg
+import json
+import logging
 
 WHITE = "#ffffff"
 PINK = "#e2979c"
@@ -8,6 +10,13 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
+
+logging.basicConfig(
+    level = logging.INFO,
+    format = '%(asctime)s [%(filename)s:%(lineno)s - %(funcName)s() ] %(levelname)s %(message)s',
+    datefmt = '%Y-%m-%d %H:%M:%S'
+    #,filename = 'basic.log'
+)
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -22,22 +31,60 @@ def generate_pass():
 def add_password():
     
     # Get current values from entry boxes
-    website = website_entry.get()
+    website = website_entry.get().lower()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {"email": email,
+                  "password": password
+        }
+    }
     
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please don't leave any fields empty!", )
     else: 
-        is_ok = messagebox.askokcancel(title=website, message=(f"These are the details entered: \nEmail: {email} \nPassword: {password} \nIs it ok to save?"))
-        
-        if is_ok:
-            print(f"{website} | {email} | {password}")
-            with open('Day29_Password_Manager\\data.txt','a') as f:
-                f.write(f"{website} | {email} | {password}\n")
-                website_entry.delete(0,END)
-                password_entry.delete(0,END)
+        # Try to open file in Read mode first
+        try:
+            with open('Day29_Password_Manager\\data.json','r') as data_file:
+                # Read the existing data from file, if it exists.
+                data = json.load(data_file)
+        except FileNotFoundError:
+            logging.error("Data json file not found. Creating one.")
+            with open('Day29_Password_Manager\\data.json','w') as data_file:
+                json.dump(new_data, data_file, indent=4)                
+        else:
+            # Update old data with new data
+            data.update(new_data)     
+            with open('Day29_Password_Manager\\data.json','w') as data_file:
+                # Saving updated data
+                json.dump(data, data_file, indent=4)
+        finally:
+            # Clear out the values in the website and password fields of the UI.
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
     
+# ---------------------------- SEARCH PASSWORDS ------------------------------- #
+
+def find_password():
+    website = website_entry.get().lower() 
+    if len(website) == 0:
+        messagebox.showerror(title="Empty Website", message="Please enter a value for the Website to search for")
+    else:
+        try:
+            with open('Day29_Password_Manager\\data.json','r') as data_file:
+                data = json.load(data_file) # Gets the contents of the file and stores it in a dictionary
+        except FileNotFoundError:
+            messagebox.showerror(title="No data file", message="No Data File Found")
+        else:
+            if website in data:
+                email = data[website]['email']
+                password = data[website]['password']
+                messagebox.showinfo(title=website, message=f"Email/Username = {email}\nPassword = {password}")
+            else:
+                messagebox.showerror(title="No match found", message="No details for the website exists.") 
+                
+
+
     
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -62,8 +109,8 @@ password_label = Label(text="Password: ", font=(FONT_NAME, 12,),)
 password_label.grid(column=1, row=4)
 
 # Text entry boxes
-website_entry = Entry(width=45)
-website_entry.grid(column=2, row=2, columnspan=2)
+website_entry = Entry(width=27)
+website_entry.grid(column=2, row=2)
 # Place cursor in this box by default
 website_entry.focus()
 email_entry = Entry(width=45)
@@ -78,6 +125,8 @@ genpass_button = Button(text="Generate Password", command=generate_pass)
 genpass_button.grid(column=3, row=4)
 add_button = Button(text="Add", width=40, command=add_password)
 add_button.grid(column=2, row=5, columnspan=2)
+search_button = Button(text="Search", command=find_password,width=14)
+search_button.grid(column=3,row=2)
 
 
 window.mainloop()
